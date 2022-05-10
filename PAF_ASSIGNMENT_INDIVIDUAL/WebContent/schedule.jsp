86834ca52de18bbb4fd9deb147a146d4a2e86949
SandaruwiniGalappaththi <sandaruwinigalappaththti@gmail.com>
2022-05-10 16:14:23 +0530

    first commit

---
+++ PAF_ASSIGNMENT_INDIVIDUAL/WebContent/schedule.jsp
@@ -0,0 +1,88 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+<title>schedule</title>
+</head>
+ 
+<body>
+<br>
+
+<div class="container">
+<img src="C:\Users\Owner\eclipse-workspace\PAF_ASSIGNMENT_INDIVIDUAL\WebContent\images\elec.jpg" alt="logo" width="60" height="50">
+  <br><br>
+  <!-- Nav pills -->
+  <ul class="nav nav-pills" role="tablist">
+    <li class="nav-item">
+      <a class="nav-link active" data-toggle="pill" href="#home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="pill" href="#menu1">Menu 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="pill" href="#menu2">Menu 2</a>
+    </li>
+  </ul>
+
+  <!-- Tab panes -->
+  <div class="tab-content">
+    <div id="home" class="container tab-pane active"><br>
+      <h3>HOME</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      
+      <div class="container">
+       <form action="/action_page.php" class="needs-validation" novalidate>
+    <div class="form-group">
+      <label for="uname">Username:</label>
+      <input type="text" class="form-control" id="uname" placeholder="Enter username" name="uname" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd" required>
+      <div class="valid-feedback">Valid.</div>
+      <div class="invalid-feedback">Please fill out this field.</div>
+    </div>
+    <div class="form-group form-check">
+      <label class="form-check-label">
+        <input class="form-check-input" type="checkbox" name="remember" required> I agree on blabla.
+        <div class="valid-feedback">Valid.</div>
+        <div class="invalid-feedback">Check this checkbox to continue.</div>
+      </label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+      </div>
+      
+      
+      
+    </div>
+    <div id="menu1" class="container tab-pane fade"><br>
+      <h3>Menu 1</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="menu2" class="container tab-pane fade"><br>
+      <h3>Menu 2</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+</body>
+</html>
---
+++ PAF_ASSIGNMENT_INDIVIDUAL/src/com/Schedule.java
@@ -0,0 +1,315 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Schedule {
+	//A common method to connect to the DB
+			private Connection connect()
+					 {
+							Connection con = null;
+					 try
+					 {
+						 	Class.forName("com.mysql.cj.jdbc.Driver");
+		
+						 	//database connection
+						 	con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/powercut", "root", "sandaru@1S");
+					 }
+					 catch (Exception e){	
+						 	e.printStackTrace();}
+					    	return con;
+					 }
+	
+	
+	
+			//method for insert power cut schedules
+			public String insertschedule(String location1, String start1, String end1, String onDate1, String createdDate1)
+					 {
+							String output = "";
+					 try
+					 {
+						 	Connection con = connect();
+						 	if (con == null)
+					 {
+						 		return "Error while connecting to the database for inserting."; }
+						 	// create a prepared statement
+						 	String query = " insert into schedules(ID,location,start,end,onDate,createdDate)" + " values (?, ?, ?, ?, ?, ?)";
+						 	PreparedStatement preparedStmt = con.prepareStatement(query);
+						 		// binding values
+						 		preparedStmt.setInt(1, 0);
+						 		preparedStmt.setString(2, location1);
+								preparedStmt.setString(3, start1);
+								preparedStmt.setString(4, end1);
+								preparedStmt.setString(5, onDate1);
+								preparedStmt.setString(6, createdDate1);
+								// execute the statement
+									preparedStmt.execute();
+									con.close();
+									output =  "<html><head><title>Payment Page</title>"
+				                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+				                            + "</head><body>"
+				                            + "<div class='card'><h4 class='text-center'style='color:green;'>Inserted schedule successfully</h4></div>"
+				                            + "</body></html>";
+					 }
+					 catch (Exception e)
+					 {
+						 	output = "<html><head><title>Payment Page</title>"
+		                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+		                            + "</head><body>"
+		                            + "<div class='card'><h4 class='text-center'style='color:red;'>Error while inserting the item.</h4></div>"
+		                            + "</body></html>";
+						 	System.err.println(e.getMessage());
+					 }
+					 return output;
+					 }
+		  
+					
+	public String readSchedule()//method for read all schedules
+			 {       
+						String output = "";
+			 try
+			 {     //checking connection
+				 	Connection con = connect();
+				 	if (con == null)
+			 {
+				 		return "Error while connecting to the database for reading."; }
+				 	// Prepare the HTML table to be displayed
+				 	output = "<html><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">"
+				 			+ "  <table border='1'><tr><th class=\"col-sm-4\"style=\"background-color:lavender;\">Location</th><th class=\"col-sm-4\"style=\"background-color:lavender;\">Start Time</th>" +
+				 				"<th class=\"col-sm-4\" style=\"background-color:lavender;\">End Time</th><th class=\"col-sm-4\" style=\"background-color:lavender;\">On Date</th>" +
+				 				"</tr>";
+                         //getting all schedule data from table
+				 		String query = "select * from schedules";
+				 		Statement stmt = con.createStatement();
+				 		ResultSet rs = stmt.executeQuery(query);
+				 		// iterate through the rows in the result set
+				 			while (rs.next())
+			 {
+				 	String ID = Integer.toString(rs.getInt("ID"));//set power cut schedule values 
+				 	String location = rs.getString("location");
+				 	String start = rs.getString("start");
+				 	String end = rs.getString("end");
+					String onDate = rs.getString("onDate");
+					
+						 // Add into the HTML table
+						 output += "<tr><td class=\"col-sm-4\" style=\"background-color:lavender;\">" + location + "</td>";
+						 output += "<td class=\"col-sm-4\" style=\"background-color:lavender;\">" + start + "</td>";
+						 output += "<td class=\"col-sm-4\" style=\"background-color:lavender;\">" + end + "</td>";
+						 output += "<td class=\"col-sm-4\" style=\"background-color:lavender;\">" + onDate + "</td>";
+						 // buttons
+						 output += "</tr>";
+			      }
+				 			con.close();
+				 			// Complete the HTML table
+				 			output += "</table></html>";
+				 					
+			    }catch(Exception e)
+			 	{
+			 		output = "Error while reading the items.";
+			 		System.err.println(e.getMessage());
+			 	}
+			 return output;
+	
+       }
+	
+	
+	
+    		public String readzone()//method for read zones
+			 {
+						String output = "";
+						try
+						{  //checking connection
+								Connection con = connect();
+								if (con == null)
+			 {
+									return "Error while connecting to the database for reading."; 
+			 }
+							 	// Prepare the HTML table to be displayed
+							 	output = "<html><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">"
+							 			+ "<table border='1'><tr>"
+							 			+"<th class=\"col-sm-4\" style=\"background-color:lavender;\">zone</th>"
+							 			+"<th class=\"col-sm-4\" style=\"background-color:lavender;\">zone</th></tr>";
+
+						 		String query = "select * from zone";
+						 		Statement stmt = con.createStatement();
+						 		ResultSet rs = stmt.executeQuery(query);
+						 		// iterate through the rows in the result set
+				 			        while (rs.next())
+				 		{
+				 
+								 	 String zone = rs.getString("zone");
+								 	 String ch = rs.getString("zone_character");
+							 	
+									 // Add into the HTML table
+									 output += "<tr><td class=\"col-sm-4\" style=\"background-color:lavender;\">" + zone + "</td>";
+									 output += "<td class=\"col-sm-4\" style=\"background-color:lavender;\">" + ch + "</td>";
+									 output += "</tr>";
+						 }
+				 			con.close();
+				 			// Complete the HTML table
+				 			output += "</table></html>";
+				 					
+			     }catch(Exception e)
+			       {
+				      output = "Error while reading the items.";
+				      System.err.println(e.getMessage());
+			       }
+			     return output;
+			 }
+			
+		
+			
+    		public String updateSchedule(String ID1,String location1,String start1,String end1,String onDate1, String createdDate1)//method for update schedules
+			{
+				 	String output = "";
+				 		try
+				 {
+				 			Boolean num =onlyDigits(ID1);
+							 if(num == false) {
+								 return  "<html><head><title>Payment Page</title>"
+				                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+				                            + "</head><body>"
+				                            + "<div class='card'><h4 class='text-center'style='color:red;'>Enter valid ID</h4></div>"
+				                            + "</body></html>";
+							 }
+				 				Connection con = connect();
+				 				if (con == null)
+				 				{	
+				 					return "Error while connecting to the database for updating."; 
+				 				}
+							 // create a prepared statement
+							 String query = "UPDATE schedules SET location=?,start=?,end=?,onDate=?,createdDate=? WHERE ID=?";
+							 PreparedStatement preparedStmt = con.prepareStatement(query);
+							 // binding values
+							 preparedStmt.setString(1, location1);
+							 preparedStmt.setString(2, start1);
+							 preparedStmt.setString(3, end1);
+							 preparedStmt.setString(4, onDate1);
+							 preparedStmt.setString(5, createdDate1 );
+							 preparedStmt.setInt(6, Integer.parseInt(ID1));
+							 // execute the statement
+							 preparedStmt.execute();
+							 con.close();
+							 
+							 output = "<html><head><title>Payment Page</title>"
+			                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+			                            + "</head><body>"
+			                            + "<div class='card'><h4 class='text-center'style='color:green;'>Updated schedule successfully</h4></div>"
+			                            + "</body></html>";
+				    }catch (Exception e)
+				     {
+					 	output = "<html><head><title>Payment Page</title>"
+	                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+	                            + "</head><body>"
+	                            + "<div class='card'><h4 class='text-center'style='color:red;'>Error while updating the item.</h4></div>"
+	                            + "</body></html>";
+					 	System.err.println(e.getMessage());
+				      }
+				return output;
+		
+			}
+	
+	
+			
+			
+			public String deleteSchedule(String ID1)//method for delete schedules
+			 {
+					String output = "";
+						try
+						{
+								Connection con = connect();
+								if (con == null)
+								{
+									return "Error while connecting to the database for deleting."; 
+									}
+								 Boolean num =onlyDigits(ID1);
+								 if(num == false) {
+									 return "<html><head><title>Payment Page</title>"
+					                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+					                            + "</head><body>"
+					                            + "<div class='card'><h4 class='text-center'style='color:red;'>Enter valid ID</h4></div>"
+					                            + "</body></html>";
+								 }
+							 // create a prepared statement
+							 String query = "delete from schedules where ID=?";
+							 PreparedStatement preparedStmt = con.prepareStatement(query);
+							 // binding values
+							 preparedStmt.setInt(1, Integer.parseInt(ID1));
+							 // execute the statement
+							 preparedStmt.execute();
+							 con.close();
+							 output ="<html><head><title>Payment Page</title>"
+			                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+			                            + "</head><body>"
+			                            + "<div class='card'><h4 class='text-center'style='color:green;'>Deleted schedule successfully</h4></div>"
+			                            + "</body></html>";
+						}catch (Exception e)
+			    
+						
+						{
+				 output = "<html><head><title>Payment Page</title>"
+                         + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+                         + "</head><body>"
+                         + "<div class='card'><h4 class='text-center'style='color:red;'>Error while deleting the item.</h4></div>"
+                         + "</body></html>";
+				 System.err.println(e.getMessage());
+					}
+			 return output;
+			 }
+		
+			
+
+			public boolean findLocation(String location) {//method for check entered location is a valid location or not
+				// TODO Auto-generated method stub
+					try
+				 		{//check connection
+					 		Connection con = connect();
+					 		if (con == null)
+					 	{
+					 		return false;
+					 		
+					 	}
+				            //get location as same as entered location
+							String query2 = "select * from zone where zone='" + location + "'"; 
+							Statement stmt2 = con.createStatement(); 
+							ResultSet rs2 = stmt2.executeQuery(query2); 
+							
+							// checking entered location is available
+							while(rs2.next()) {
+								return true;
+							
+						      }
+			
+
+				 		}catch(Exception e) {
+	 
+				
+				
+		    }
+				return false;
+				
+        }
+			public static boolean onlyDigits(String str)
+		    {
+				
+		        for (int i = 0; i < str.length();i++) {
+		  
+		            // Check if character is
+		            // digit from 0-9
+		            // then return true
+		            // else false
+		            if (str.charAt(i) >= '0'
+		                && str.charAt(i) <= '9') {
+		                return true;
+		            }
+		            else {
+		                return false;
+		            }
+		        }
+		        return false;
+		    }
+		  
+}
